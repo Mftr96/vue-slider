73d9a81cf40a77c1dcfba48061431433336d413b
Mftr96 <matteo.federici96@gmail.com>
2024-05-13 23:01:19 +0200

    bonus 1

--- js/script.js
+++ js/script.js
@@ -5,27 +5,33 @@ const{createApp}=Vue;
 createApp({
     data(){
         return{
+            //bonus 1 :aggiungo contatori per funzione 
             slides: [
                 {
                     image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    contatore:0    
                 }, {
                     image: 'img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    contatore:1
                 }, {
                     image: 'img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    contatore:2
                 }, {
                     image: 'img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    contatore:3
                 }, {
                     image: 'img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    contatore:4
                 }
             ],
             
@@ -58,7 +64,16 @@ createApp({
             }else if(this.attiva<=0){
              this.attiva=4
             }
+         },
+
+         fThumbClick(i){
+            this.attiva=i
          }
       
     }
 }).mount("#app");
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
